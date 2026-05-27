589bd59c2fad59999a7a6895f8c2bbbc3bd77668
GalaMicaelaGutierrezBasso <galagutierrez3.5@gmail.com>
2026-05-27 17:34:17 -0300

    js
    script.js

---
+++ practica-17/js/script.js
@@ -0,0 +1,5 @@
+let precio = 100
+let descuento = 20
+console.log('Precio sin descuento es $' + precio)
+console.log('El descuento es' + ' ' + descuento + '%')
+console.log('Precio con descuento es $' + (precio - (precio*(descuento/100))))
