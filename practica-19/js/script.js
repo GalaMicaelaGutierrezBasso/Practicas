f8348534102761e3fb0daeda27c3e335b2ba17b8
GalaMicaelaGutierrezBasso <galagutierrez3.5@gmail.com>
2026-06-09 13:12:41 -0300

    js
    script.js

---
+++ practica-19/js/script.js
@@ -0,0 +1,24 @@
+let titulo = document.querySelector('h1');
+titulo.addEventListener ('click', function () {
+    titulo.style.backgroundColor = 'red';
+});
+let subtitulo = document.querySelector('h2');
+subtitulo.addEventListener ('click', function () {
+    subtitulo.style.backgroundColor = 'red';
+});
+let parrafouno = document.querySelector('#uno');
+parrafouno.addEventListener ('click', function () {
+    parrafouno.style.backgroundColor = 'red';
+});
+let parrafodos = document.querySelector('#dos');
+parrafodos.addEventListener ('click', function () {
+    parrafodos.style.backgroundColor = 'red';
+});
+let lista = document.querySelector('div');
+lista.addEventListener ('click', function () {
+    lista.style.backgroundColor = 'red';
+});
+let imagen = document.querySelector('img');
+imagen.addEventListener ('click', function () {
+imagen.style.display = 'none';
+});
