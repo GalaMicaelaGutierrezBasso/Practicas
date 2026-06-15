5823a6786d5ffc055e23478c8427188f94b8239d
GalaMicaelaGutierrezBasso <galagutierrez3.5@gmail.com>
2026-06-15 17:54:26 -0300

    practica-21
    script.js

--- practica-21/js/script.js
+++ practica-21/js/script.js
@@ -1,8 +1,27 @@
-let confirmar = confirm ('personalizar bienvenida')
+let confirmar = confirm('¿Desea personalizar su bienvenida?');
 if (confirmar) {
     let nombre = prompt('¿Cuál es tu nombre?');
-    document.querySelector('h1').textContent = 'Bienvenidx,' + ' ' + nombre;
-}
-else {
+    document.querySelector('h1').textContent = 'Bienvenidx, ' + nombre;
+} else {
     document.querySelector('h1').textContent = 'Bienvenidx!!!';
+}
+confirmar = confirm('¿Desea cambiar el modo de visualización de la página?');
+if (confirmar) {
+    document.querySelector('body').style.backgroundColor = 'black';
+} else {
+    document.querySelector('body').style.backgroundColor = 'white';
+}
+let hora = new Date().getHours();
+let saludo = 'h1';
+if (hora < 12) {
+    saludo = 'Buenos días';
+} else if (hora < 18) {
+    saludo = 'Buenas tardes';
+} else {
+    saludo = 'Buenas noches';
+}
+if (nombre) {
+    document.querySelector('h1').textContent = saludo + ', ' + nombre;
+} else {
+    document.querySelector('h1').textContent = saludo + '!!!';
 }
